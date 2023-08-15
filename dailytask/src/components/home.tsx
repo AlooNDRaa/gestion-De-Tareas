5ae54c942551951d19e91e6983f434dcc2a9f057
Martina G.V <mgarciavior@gmail.com>
2023-08-15 10:25:53 -0300

    upload react proyect folder

---
+++ dailytask/src/components/home.tsx
@@ -0,0 +1,19 @@
+import './home.css'
+function NavBar() {
+    return (
+        <>        
+            <nav>
+                <img src="" alt="logo"/>
+                <a>Tableros</a>
+                <button className='crear'>Crear</button>
+                <div className='icons'>
+                    <a></a>
+                    <a></a>
+                    <a></a>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export { NavBar }
