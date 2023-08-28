cd1863403944a417e3f8438beec41fbab5a7b440
FlorenciaPereyra <pereyraramosflorenciacamila@gmail.com>
2023-08-27 21:42:07 -0300

    Refactor: Estadisticas.

---
+++ Estadisticas/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import Stats from './styles'
+
+
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+  <Stats/>
+
+  </> 
+  )
+}
+
+export default App
