2075d948b0f02f9f2f00a5d05359eccdb8b43631
FlorenciaPereyra <pereyraramosflorenciacamila@gmail.com>
2023-09-05 09:48:07 -0300

    Refactor: DataInput.

--- frontend/src/assets/pages/stadistics.tsx
+++ frontend/src/assets/pages/stadistics.tsx
@@ -21,15 +21,15 @@ const Stats: React.FC = () => {
         
             <div className='mt-10'>
             <button className='text-darkmode-verdeagua1 mr-20'>Ver estadísticas</button>
-              <button className='bg-darkmode-verdeagua1 items-end rounded-tl-lg rounded-bl-lg p-1 mr-0.5'>Semana pasada</button>
-              <button className='bg-darkmode-verdeagua1 items-end p-1 mr-0.5'>Mes pasado</button>
-              <button className='bg-darkmode-verdeagua1 items-end rounded-br-lg rounded-tr-lg p-1 mr-20'>Último año</button>
-              <button className='bg-darkmode-verdeagua1 items-end rounded-lg p-1'>Junio 2023</button>
+              <button className='bg-darkmode-verdeagua1 items-end rounded-tl-lg rounded-bl-lg p-1 mr-0.5 hover:bg-darkmode-verdeagua2'>Semana pasada</button>
+              <button className='bg-darkmode-verdeagua1 items-end p-1 mr-0.5 hover:bg-darkmode-verdeagua2'>Mes pasado</button>
+              <button className='bg-darkmode-verdeagua1 items-end rounded-br-lg rounded-tr-lg p-1 mr-20 hover:bg-darkmode-verdeagua2'>Último año</button>
+              <button className='bg-darkmode-verdeagua1 items-end rounded-lg p-1 hover:bg-darkmode-verdeagua2'>Junio 2023</button>
             </div>
             </div>
 
-              <div className='flex justify-center m-auto w-4/6 h-4/5'>
-              <div className="fit-content inline-block rounded bg-darkmode-azul3 flex justify-center w-full p-10 mt-20">
+              <div className='flex justify-center mt-10 rounded flex'>
+              <div className="flex justify-center p-10 bg-darkmode-azul3 min-w-3/5">
               <LinesCharts/>
             </div> 
             </div>
