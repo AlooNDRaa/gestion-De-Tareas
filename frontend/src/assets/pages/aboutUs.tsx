ae1a9711720ebd4ee472692d72d11b7a9d37cf30
Alondra <alonngadea@gmail.com>
2023-09-13 11:51:53 -0300

    Refactor: About us page, with links

--- frontend/src/assets/pages/aboutUs.tsx
+++ frontend/src/assets/pages/aboutUs.tsx
@@ -2,6 +2,8 @@ import { NavBar, NavBarMobile } from "../components/navbar";
 import { useState } from 'react';
 import { BsGithub } from 'react-icons/bs';
 import { FiFigma, FiTrello } from 'react-icons/fi'
+import { GiReturnArrow } from 'react-icons/gi'
+import { Link } from "react-router-dom";
 
 
 export function AboutUsPage() {
@@ -12,34 +14,39 @@ export function AboutUsPage() {
             <div className={`${theme ? 'text-darkmode-verdeagua1' : 'text-lightmode-azul'}`}>
                 <NavBar theme={theme} changeTheme={() => { setTheme(!theme) }} />
                 <NavBarMobile />
-                <div className={`${theme ? 'bg-[#053B50]' : 'bg-lightmode-blanco'} w-full h-screen flex justify-evenly text-center`}>
-                        <div className={`w-1/3 p-6g`}>
-                        <h1 className="text-[5rem] m-7">About Us</h1>
+                <div className={`${theme ? 'bg-[#031124]' : 'bg-[#dff5ed]'} h-screen flex justify-between `}>
+                        <div className={`grid w-1/3 p-2 ml-16 text-center`}>
+                        <h1 className="text-[5rem] m-auto">About Us</h1>
                             <p className="text-[1.5rem]">
-                                We are a group of programming students who seek to make this project a more interactive and unique experience for the user.
-                                Each of us contributed ideas that were used for the development of this task manager.
-                                Each section of our project has its outstanding characteristics.
+                            We are a group of programming students who seek to make this project a more interactive and unique experience for the user. Each of us contributed ideas that were used for the development of this task manager. Each section of our project has its outstanding features.
+                            <br/>
+                             We do not have a physical company but you can find us at these links or back home:
                             </p>
                             </div>
-                            <div className="flex justify-center items-center flex-col gap-5">
-                                <button>
-                                    <a href="https://github.com/AlooNDRaa/gestion-De-Tareas">
-                                        <BsGithub size='60px' />
-                                    </a>
-                                </button>
-                                <button>
-                                    <a href="https://www.figma.com/file/eTrdxntwqUY4GHw37umfsb/Gestor-de-tareas?type=design&node-id=0-1&mode=design&t=mnEqt6pQn7NfzsjB-0">
-                                        <FiFigma  size='60px'/>
-                                    </a>
-                                </button>
-                                <button>
-                                    <a href="https://trello.com/b/dyA9asgL/equipo-4-trello">
-                                        <FiTrello   size='60px'/>
-                                    </a>
-                                </button>
+                            <div className={`${theme ? 'bg-[#001C30]' : 'bg-[#ADC4CE]'} flex w-1/2`}>
+                     <div className="items-center flex flex-col gap-7 m-auto">
+                        <button className="animate-pulse hover:animate-spin">
+                        <a href="https://github.com/AlooNDRaa/gestion-De-Tareas">
+                            <BsGithub size='60px' />
+                        </a>
+                        </button>
+                        <button className="animate-pulse hover:animate-spin">
+                        <a href="https://www.figma.com/file/eTrdxntwqUY4GHw37umfsb/Gestor-de-tareas?type=design&node-id=0-1&mode=design&t=mnEqt6pQn7NfzsjB-0">
+                            <FiFigma  size='60px'/>
+                        </a>
+                        </button>
+                        <button className="animate-pulse hover:animate-spin">
+                        <a href="https://trello.com/b/dyA9asgL/equipo-4-trello">
+                            <FiTrello   size='60px'/>
+                        </a>
+                        </button>
+                        <button className="animate-pulse hover:animate-spin">
+                          <Link to="/Home"> <GiReturnArrow   size='60px'/> </Link>
+                        </button>
+                    </div>
+                    </div>
                         </div>
                     </div>
-                </div>
         </>
     )
 }
